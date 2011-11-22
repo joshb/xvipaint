6270bc56e557c47b4fff139af2b398d624cbe29c
Josh Beam <josh@joshbeam.com>
2011-11-22 10:11:10 -0600

    Changed the listening address to 0.0.0.0.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd `dirname $0`/paint
-../build/xviweb/src/xviweb/xviweb --loadResponder ../build/xviweb/src/FileResponder/libFileResponder.so --loadResponder ../build/src/libxvipaint.so --defaultRoot .
+../build/xviweb/src/xviweb/xviweb --address 0.0.0.0 --loadResponder ../build/xviweb/src/FileResponder/libFileResponder.so --loadResponder ../build/src/libxvipaint.so --defaultRoot .
