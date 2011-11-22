6b7bc2ef6656914d0ffd6fdb5e4718e334ae54cb
Josh Beam <josh@joshbeam.com>
2011-11-22 17:28:50 -0600

    Created the www directory and moved the paint directory to it.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd `dirname $0`/paint
-../build/xviweb/src/xviweb/xviweb --address 0.0.0.0 --loadResponder ../build/xviweb/src/FileResponder/libFileResponder.so --loadResponder ../build/src/libxvipaint.so --defaultRoot .
+cd `dirname $0`/www/paint
+../../build/xviweb/src/xviweb/xviweb --address 0.0.0.0 --loadResponder ../../build/xviweb/src/FileResponder/libFileResponder.so --loadResponder ../../build/src/libxvipaint.so --defaultRoot ..
